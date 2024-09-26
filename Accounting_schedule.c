0ce0bd82792fd3a63139143bf841d512ba6dec7f
darkelvesasd <96123329+darkelvesasd@users.noreply.github.com>
2024-09-26 15:46:56 +0800

    Update Accounting_schedule.c

--- Accounting_schedule.c
+++ Accounting_schedule.c
@@ -2,12 +2,12 @@
   2 #include<unistd.h>
   3 int main()
   4 {
-  5     int n=9;
+  5     int n=10;
   6     while(n)
   7     {
-  8         printf("%d\r",n--);
+  8         printf("%2d\r",n--);                                                                                                                                                           
   9         fflush(stdout);
  10         sleep(1);
  11     }
- 12     return 0;                                                                                                                                                                          
+ 12     return 0;
  13 }
